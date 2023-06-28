c7144fdf2584ce0ff4706491e765e4bad043a6b5
mrx_fr1ends <prosto.pasha123@gmail.com>
2023-06-28 13:41:23 +0300

    Add navigation header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavouritesPage from "./pages/FavouritesPage";
+import Navigation from "./components/Navigation";
+
 function App() {
-  return (
-    <div className="App">
-      <h1 className="font-bold">Hello React</h1>
-    </div>
-  );
+    return (
+        <>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favourites" element={<FavouritesPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/FavouritesPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavouritesPage = () => {
+    return (
+        <div>
+            Fav
+        </div>
+    );
+};
+
+export default FavouritesPage;
